ed1752c79a38471467418bf8b0a90c634a78a226
Yousif Sabah <iyousifsabah@gmail.com>
2025-07-05 13:11:23 +0300

    more tests

--- cmd/snippets/templates_test.go
+++ cmd/snippets/templates_test.go
@@ -6,11 +6,34 @@ import (
 )
 
 func TestHumanDate(t *testing.T) {
-	tm := time.Date(2025, 7, 5, 2, 15, 0, 0, time.UTC)
-	hd := humanDate(tm)
+	tests := []struct {
+		name string
+		tm   time.Time
+		want string
+	}{
+		{
+			name: "UTC",
+			tm:   time.Date(2025, 7, 5, 2, 15, 0, 0, time.UTC),
+			want: "05 Jul 2025 at 02:15",
+		},
+		{
+			name: "Empty",
+			tm:   time.Time{},
+			want: "",
+		},
+		{
+			name: "CET",
+			tm:   time.Date(2025, 7, 5, 2, 15, 0, 0, time.FixedZone("CET", 1*60*60)),
+			want: "05 Jul 2025 at 01:15",
+		},
+	}
 
-	want := "05 Jul 2025 at 02:15"
-	if hd != want {
-		t.Errorf("got %q; want %q", hd, want)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			hd := humanDate(test.tm)
+			if hd != test.want {
+				t.Errorf("want %q; got %q", test.want, hd)
+			}
+		})
 	}
 }
